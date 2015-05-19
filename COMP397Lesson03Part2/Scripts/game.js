ae03501049ed25435af3e7a513c3f9ebbe35deb6
AliSaim <alisaimwindows@gmail.com>
2015-05-19 10:45:58 -0400

    added more sildes

---
+++ COMP397Lesson03Part2/Scripts/game.js
@@ -0,0 +1,7 @@
+/// <reference path="typings/impress/impress.d.ts" />
+function init() {
+    //impress().init();
+    console.log("Hello test ag==pag... game is runnng");
+    impress().init();
+}
+//# sourceMappingURL=game.js.map
